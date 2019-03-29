317b6aceac94b68dc2184fbb13600d27bd031755
Alexis Hernandez Perez <1717110754@utectulancingo.edu.mx>
2019-03-28 20:50:11 -0600

    Forma remota

--- application/models/config.py
+++ application/models/config.py
@@ -2,11 +2,12 @@ import web
 
 
 
+
 db = web.database(
     dbn='mysql',
-    host='localhost',
-    db=  'ferreteria_ahp1',
-    user= 'root',
-    pw= '1234',
-    port=3308
+    host='c9cujduvu830eexs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    db=  'qm3o50s9k6heqlrv',
+    user= 'yv3povojvet8sloq',
+    pw= 'lqakkunhz3vl4kt8',
+    port=3306
     )
