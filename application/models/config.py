e54e5b31620f223f357fb7f546ccd074a581fb82
Alexis Hernandez Perez <1717110754@utectulancingo.edu.mx>
2019-03-28 20:17:49 -0600

    WebServiceFeribios

---
+++ application/models/config.py
@@ -0,0 +1,12 @@
+import web
+
+
+
+db = web.database(
+    dbn='mysql',
+    host='localhost',
+    db=  'ferreteria_ahp1',
+    user= 'root',
+    pw='1234',
+    port=3308
+    )
---
+++ config.py
@@ -0,0 +1,14 @@
+import web
+
+
+print "Conectar"
+db = web.database(
+    dbn='mysql',
+    host='localhost',
+    db=  'ferreteria_ahp1',
+    user= 'root',
+    pw='1234',
+    port=3308
+    )
+    
+print "Conectado"
