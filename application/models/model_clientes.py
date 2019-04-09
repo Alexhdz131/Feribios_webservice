6c470f8bee74c866c733beaa5f65e00e0f43c592
israel merida lopez <1717110625@utectulancingo.edu.mx>
2019-04-08 20:45:11 -0500

    modificacion de models

--- application/models/model_clientes.py
+++ application/models/model_clientes.py
@@ -6,53 +6,53 @@ db = config.db
 
 def get_all_clientes():
     try:
-        return db.select('clientes')
+        return db.select('evento')
     except Exception as e:
         print "Model get all Error {}".format(e.args)
         print "Model get all Message {}".format(e.message)
         return None
 
 
-def get_clientes(id_cliente):
+def get_clientes(id_evento):
     try:
-        return db.select('clientes', where='id_cliente=$id_cliente', vars=locals())[0]
+        return db.select('evento', where='id_evento=$id_evento', vars=locals())[0]
     except Exception as e:
         print "Model get Error {}".format(e.args)
         print "Model get Message {}".format(e.message)
         return None
 
 
-def delete_clientes(id_cliente):
+def delete_clientes(id_evento):
     try:
-        return db.delete('clientes', where='id_cliente=$id_cliente', vars=locals())
+        return db.delete('evento', where='id_evento=$id_evento', vars=locals())
     except Exception as e:
         print "Model delete Error {}".format(e.args)
         print "Model delete Message {}".format(e.message)
         return None
 
 
-def insert_clientes(nombre,apellido_paterno,apellido_materno,telefono,email):
+def insert_evento(titulo,descripcion,fecha,hora,ubicacion):
     try:
-        return db.insert('clientes',nombre=nombre,
-apellido_paterno=apellido_paterno,
-apellido_materno=apellido_materno,
-telefono=telefono,
-email=email)
+        return db.insert('evento',titulo=titulo,
+descripcion=descripcion,
+fecha=fecha,
+hora=hora,
+ubicacion=ubicacion)
     except Exception as e:
         print "Model insert Error {}".format(e.args)
         print "Model insert Message {}".format(e.message)
         return None
 
 
-def edit_clientes(id_cliente,nombre,apellido_paterno,apellido_materno,telefono,email):
+def edit_evento(id_evento,titulo,descripcion,fecha,hora,ubicacion):
     try:
-        return db.update('clientes',id_cliente=id_cliente,
-nombre=nombre,
-apellido_paterno=apellido_paterno,
-apellido_materno=apellido_materno,
-telefono=telefono,
-email=email,
-                  where='id_cliente=$id_cliente',
+        return db.update('evento',id_evento=id_evento,
+titulo=titulo,
+descripcion=descripcion,
+fecha=fecha,
+hora=hora,
+ubicacion=ubicacion
+                  where='id_evento=$id_evento',
                   vars=locals())
     except Exception as e:
         print "Model update Error {}".format(e.args)
