7c528f2e88110d53953dd0066f921d2c57584d99
Alexis Hernandez Perez <1717110754@utectulancingo.edu.mx>
2019-04-09 09:40:02 -0500

    Servidor Remoto

--- application/models/config.py
+++ application/models/config.py
@@ -5,9 +5,9 @@ import web
 
 db = web.database(
     dbn='mysql',
-    host='localhost',
-    db=  'craftsystem',
-    user= 'root',
-    pw= '1234',
-    port=3308
+    host='c9cujduvu830eexs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    db=  'ufieql1f90kzz50h',
+    user= 'dox3t6g7obt4dj40',
+    pw= 'wqnj186edmmgwcvw',
+    port=3306
     )
