be7ec76bdb0ede19691443644c9923185c54c986
Alexis Hernandez Perez <1717110754@utectulancingo.edu.mx>
2019-04-09 09:43:34 -0500

    Servidor Remoto

--- application/models/config.py
+++ application/models/config.py
@@ -9,5 +9,5 @@ db = web.database(
     db=  'ufieql1f90kzz50h',
     user= 'dox3t6g7obt4dj40',
     pw= 'wqnj186edmmgwcvw',
-    port=3306
+    port='3306'
     )
--- config.py
+++ config.py
@@ -10,6 +10,6 @@ db = web.database(
     db=  'ufieql1f90kzz50h',
     user= 'dox3t6g7obt4dj40',
     pw= 'wqnj186edmmgwcvw',
-    port=3306
+    port='3306'
     )
 print "Conectado"
