daeea7e9520ba5159c6239d7d8b3c6200453e0db
israel merida lopez <1717110625@utectulancingo.edu.mx>
2019-04-08 19:37:20 -0500

    modificacion de view (view,master,insert,index,edit,delete) y json

--- application/api/clientes/api_clientes.py
+++ application/api/clientes/api_clientes.py
@@ -7,7 +7,7 @@ class Api_clientes:
     def get(self, id_cliente):
         try:
             # http://localhost:8080/api_clientes?user_hash=12345&action=get
-            if id_cliente is None:
+            if id_evento is None:
                 result = config.model.get_all_clientes()
                 clientes_json = []
                 for row in result:
@@ -17,7 +17,7 @@ class Api_clientes:
                 return json.dumps(clientes_json)
             else:
                 # http://localhost:8080/api_clientes?user_hash=12345&action=get&id_cliente=1
-                result = config.model.get_clientes(int(id_cliente))
+                result = config.model.get_clientes(int(id_evento))
                 clientes_json = []
                 clientes_json.append(dict(result))
                 web.header('Content-Type', 'application/json')
@@ -29,9 +29,9 @@ class Api_clientes:
             return json.dumps(clientes_json)
 
 # http://localhost:8080/api_clientes?user_hash=12345&action=put&id_cliente=1&product=nuevo&description=nueva&stock=10&purchase_price=1&price_sale=3&product_image=0
-    def put(self, nombre,apellido_paterno,apellido_materno,telefono,email):
+    def put(self, titulo, description, fecha, hora, ubicacion ):
         try:
-            config.model.insert_clientes(nombre,apellido_paterno,apellido_materno,telefono,email)
+            config.model.insert_clientes(titulo, description, fecha, hora, ubicacion)
             clientes_json = '[{200}]'
             web.header('Content-Type', 'application/json')
             return json.dumps(clientes_json)
@@ -40,9 +40,9 @@ class Api_clientes:
             return None
 
 # http://localhost:8080/api_clientes?user_hash=12345&action=delete&id_cliente=1
-    def delete(self, id_cliente):
+    def delete(self, id_evento):
         try:
-            config.model.delete_clientes(id_cliente)
+            config.model.delete_clientes(id_evento)
             clientes_json = '[{200}]'
             web.header('Content-Type', 'application/json')
             return json.dumps(clientes_json)
@@ -51,9 +51,9 @@ class Api_clientes:
             return None
 
 # http://localhost:8080/api_clientes?user_hash=12345&action=update&id_cliente=1&product=nuevo&description=nueva&stock=10&purchase_price=1&price_sale=3&product_image=default.jpg
-    def update(self, id_cliente, nombre,apellido_paterno,apellido_materno,telefono,email):
+    def update(self, id_evento, titulo, description, fecha, hora, ubicacion):
         try:
-            config.model.edit_clientes(id_cliente,nombre,apellido_paterno,apellido_materno,telefono,email)
+            config.model.edit_clientes(id_evento,titulo,description,fecha,hora,ubicacion)
             clientes_json = '[{200}]'
             web.header('Content-Type', 'application/json')
             return json.dumps(clientes_json)
@@ -67,40 +67,37 @@ class Api_clientes:
         user_data = web.input(
             user_hash=None,
             action=None,
-            id_cliente=None,
-            nombre=None,
-            apellido_paterno=None,
-            apellido_materno=None,
-            telefono=None,
-            email=None,
+            id_evento=None,
+            titulo=None,
+            description=None,
+            fecha=None,
+            hora=None,
         )
         try:
             user_hash = user_data.user_hash  # user validation
             action = user_data.action  # action GET, PUT, DELETE, UPDATE
-            id_cliente=user_data.id_cliente
+            id_evento=user_data.id_evento
 
-            nombre=user_data.nombre
+            titulo=user_data.titulo
 
-            apellido_paterno=user_data.apellido_paterno
+            description=user_data.description
 
-            apellido_materno=user_data.apellido_materno
+            fecha=user_data.fecha
 
-            telefono=user_data.telefono
-
-            email=user_data.email
+            hora=user_data.hora
 
             # user_hash
             if user_hash == '12345':
                 if action is None:
                     raise web.seeother('/404')
                 elif action == 'get':
-                    return self.get(id_cliente)
+                    return self.get(id_evento)
                 elif action == 'put':
-                    return self.put(nombre,apellido_paterno,apellido_materno,telefono,email)
+                    return self.put(titulo,description,fecha,hora,ubicacion)
                 elif action == 'delete':
-                    return self.delete(id_cliente)
+                    return self.delete(id_evento)
                 elif action == 'update':
-                    return self.update(id_cliente, nombre,apellido_paterno,apellido_materno,telefono,email)
+                    return self.update(id_evento,titulo,description,fecha,hora,ubicacion)
             else:
                 raise web.seeother('/404')
         except Exception as e:
