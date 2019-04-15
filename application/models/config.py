782763a269974b67f7a8a0798117dcecd2a48704
Alexis Hernandez Perez <1717110754@utectulancingo.edu.mx>
2019-04-15 08:35:39 -0500

    Actualizacion

--- application/models/config.py
+++ application/models/config.py
@@ -2,13 +2,13 @@ import web
 
 
 
-"""
+
 db = web.database(
     dbn='mysql',
-    host='c9cujduvu830eexs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    host='rtzsaka6vivj2zp1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     db=  'ufieql1f90kzz50h',
-    user= 'dox3t6g7obt4dj40',
-    pw= 'wqnj186edmmgwcvw',
+    user= 'g9clfaggz1t1r8p6',
+    pw= 'ev2ashyxooy7cgls',
     port=3306
     )
 """
@@ -21,4 +21,4 @@ db = web.database(
     pw= '1234',
     port=3306
     )
-    
+    """
